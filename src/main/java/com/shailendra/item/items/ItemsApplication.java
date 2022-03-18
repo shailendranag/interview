1d18cc801d00dd957d99dc3a409dae58874a57f3
nag <sailunag18@gmail.com>
2022-03-18 16:58:51 +0530

    Adds Eureka Registering Capability

--- src/main/java/com/shailendra/item/items/ItemsApplication.java
+++ src/main/java/com/shailendra/item/items/ItemsApplication.java
@@ -3,6 +3,7 @@ package com.shailendra.item.items;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.autoconfigure.domain.EntityScan;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
 
@@ -10,6 +11,7 @@ import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
 @ComponentScan("com.shailendra.item.*")
 @EnableJpaRepositories("com.shailendra.item.*")
 @EntityScan("com.shailendra.item.*")
+@EnableDiscoveryClient
 public class ItemsApplication {
 
 	public static void main(String[] args) {
